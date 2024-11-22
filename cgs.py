e92112f21f5aac3d095d80f8548d46a26cda31e6
Garret <83665593+GarretJT@users.noreply.github.com>
2024-11-22 13:30:12 +0700

    Update cgs.py

--- cgs.py
+++ cgs.py
@@ -15,7 +15,24 @@ tickers = [
     "NICK.JK", "APLI.JK", "ATIC.JK", "SHIP.JK", "DCII.JK", "MEGA.JK", "YULE.JK", 
     "PTSP.JK", "TRUS.JK", "SAPX.JK", "DAYA.JK", "SKBM.JK", "EDGE.JK", "MERK.JK", 
     "TBMS.JK", "RANC.JK", "HDFA.JK", "GHON.JK", "SOTS.JK", "BINA.JK", "LINK.JK", 
-    "PURI.JK", "IFSH.JK", "SIPD.JK", "KINO.JK" , "MCAS.JK"
+    "PURI.JK", "IFSH.JK", "SIPD.JK", "KINO.JK" , "MCAS.JK", 
+    "INRU.JK", "CBUT.JK", "APII.JK", "ASRM.JK", "GHON.JK", "HERO.JK", "LCKM.JK", 
+    "RELI.JK", "TIRA.JK", "TCID.JK", "TGKA.JK", "MASB.JK", "TGKA.JK", "BBMD.JK", 
+    "GEMA.JK", "BBLD.JK", "RSGK.JK", "BBSI.JK", "KEJU.JK", "TRST.JK", "PNGO.JK", 
+    "LIFE.JK", "LINK.JK", "PGLI.JK", "GLVA.JK", "INDR.JK", "BPFI.JK", "BRAM.JK", 
+    "SOTS.JK", "SDRA.JK", "ASJT.JK", "ARGO.JK", "MORA.JK", "ALDO.JK", "INTD.JK", 
+    "POLU.JK", "BINO.JK", "MMLP.JK", "LPLI.JK", "BALI.JK", "BOGA.JK", "MTLA.JK", 
+    "BUKK.JK", "AMIN.JK", "GMTD.JK", "SHID.JK", "BTON.JK", "BBLD.JK", "APLI.JK", 
+    "NICK.JK", "MPRO.JK", "TALF.JK", "CSAP.JK", "TRUK.JK", "IPAC.JK", "JECC.JK", 
+    "FUJI.JK", "IKBI.JK", "MICE.JK", "BPII.JK", "LCKM.JK", "MGLV.JK", "SGRO.JK", 
+    "TRIS.JK", "KEJU.JK", "PEHA.JK", "AMOR.JK", "BMSR.JK", "TIRA.JK", "SKBM.JK", 
+    "SURE.JK", "BSIM.JK", "ALKA.JK", "LINK.JK", "EDGE.JK", "MRAT.JK", "SCNP.JK", 
+    "DEPO.JK", "CASH.JK", "BMHS.JK", "INPP.JK", "BBSS.JK", "AGAR.JK", "BINA.JK", 
+    "SHIP.JK", "CEKA.JK", "AMFG.JK", "BABY.JK", "CINT.JK", "IDPR.JK", "HDFA.JK", 
+    "GOLD.JK", "IFSH.JK", "SAFE.JK", "DAYA.JK", "INAI.JK", "ASBI.JK", "PTSP.JK", 
+    "SHIP.JK", "SIPD.JK", "TBMS.JK", "LFLO.JK", "BESS.JK", "RANC.JK", "GDYR.JK", 
+    "LCKM.JK", "PDES.JK", "IMJS.JK", "ASDM.JK", "OILS.JK", "TRUS.JK", "INCI.JK", 
+    "SMMA.JK"
 ]
 
 # Remove duplicates
